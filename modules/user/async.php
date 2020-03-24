f267999916e5f2fc2767632fe6be0665589e1b7b
Danang Widiantoro <danang@fisip.net>
2020-03-24 14:48:22 +0700

    execute async synchronously

--- modules/user/async.php
+++ modules/user/async.php
@@ -10,6 +10,14 @@ if (!empty($id))
 			$db->Execute("DELETE FROM `bbc_async` WHERE `id`={$id}");
 			break;
 
+		case 'last':
+			$out = array(
+				'ok'     => 1,
+				'lastID' => intval($db->getOne("SELECT `id` FROM `bbc_async` WHERE 1 ORDER BY `id` ASC"))
+				);
+			output_json($out);
+			break;
+
 		default:
 			_class('async')->fix($id);
 			break;
@@ -30,10 +38,11 @@ if (!empty($exist))
 	$add_sql = $form->search->action();
 	$keyword = $form->search->keyword();
 
-	echo $form->search->getForm();
+	$searchForm =  $form->search->getForm();
 
 	$form->initRoll("{$add_sql} ORDER BY id ASC");
 
+	// $form->roll->setNumRows(2);
 	$form->roll->setSaveTool(false);
 	$form->roll->setDeleteTool(true);
 
@@ -45,6 +54,7 @@ if (!empty($exist))
 	$form->roll->input->idx->setFieldName('id AS idx');
 	$form->roll->input->idx2->setFieldName('id AS idx2');
 	$form->roll->input->idx2->setLinks($Bbc->mod['circuit'].'.'.$Bbc->mod['task'], icon('console').' Execute');
+	$form->roll->input->idx2->setExtra(' class="executeOne"');
 	// $form->roll->input->idx2->setLinks(
 	// 	array(
 	// 	$Bbc->mod['circuit'].'.'.$Bbc->mod['task']            => icon('console').' Execute',
@@ -70,7 +80,79 @@ if (!empty($exist))
 	$form->roll->action();
 	?>
 	<div class="container-fluid">
-		<?php echo $form->roll->getForm(); ?>
+		<?php
+		if (empty($_GET['page']) || @$_GET['page']==1)
+		{
+			?>
+			<button type="button" class="btn btn-default btn-sm pull-left" id="executeAll">Execute All</button>
+			<script type="text/javascript">
+				function executesync(x) {
+					$.ajax({
+						url: _URL+"user/async?act=last&id="+x,
+						dataType:"json",
+						success: function(a){
+							if (a.ok) {
+								if (a.lastID) {
+									var lastID = sessionStorage.getItem("executeAll");
+									if (lastID == a.lastID) {
+										$("#executeAll").text('Checking..');
+										setTimeout(function() {
+											executesync(x);
+										}, 1000);
+									}else{
+										var url = document.location.href;
+										if (url.substr(-1) == "/") {
+											url += a.lastID+"?return=";
+										}else{
+											url += url.indexOf("?") >= 0 ? "&" : "?";
+											url += "id="+a.lastID+"&return=";
+										}
+										url += encodeURI(document.location.href);
+										$("#executeAll").text('Loading...');
+										sessionStorage.setItem("executeAll", a.lastID);
+										document.location.href = url;
+									}
+								}else{
+									sessionStorage.setItem("executeAll", 0);
+									document.location.reload();
+								}
+							}
+						}
+					});
+				};
+				_Bbc(function($){
+					if (typeof(Storage) !== "undefined") {
+						$("#executeAll").on("click", function(e){
+							e.preventDefault();
+							var a = $(".executeOne");
+							if (a.length > 0) {
+								var b = a.attr("href").match(/\/async\/([0-9]+)/);
+								sessionStorage.setItem("executeAll", b[1]);
+								$(this).text('Loading...');
+								document.location.href = a.attr("href");
+							}else{
+								sessionStorage.setItem("executeAll", 0);
+								$(this).hide();
+							}
+						});
+					} else {
+						$(this).hide();
+					}
+					// jika ada data di session
+					var c = sessionStorage.getItem("executeAll");
+					if (c > 0) {
+						executesync(c);
+					}
+					if ($(".executeOne").length == 0) {
+						$("#executeAll").hide();
+					}
+				});
+			</script>
+			<?php
+		}
+		echo $searchForm;
+		echo $form->roll->getForm();
+		?>
 	</div>
 	<?php
 }
