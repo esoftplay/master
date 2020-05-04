95154e9e09125ec0f5556b43adda96d51702cccd
syafiq <syafiq@fisip.net>
2020-05-04 12:57:09 +0700

    App Icon Badge Number (iOS)

--- includes/function/alert.php
+++ includes/function/alert.php
@@ -352,6 +352,8 @@ function alert_push_send($id, $last_id=0)
 	$data   = $db->getRow("SELECT * FROM `bbc_user_push_notif` WHERE id={$id}");
 	if (!empty($data))
 	{
+		$unread   = $db->getOne("SELECT count(id) FROM `bbc_user_push_notif` WHERE `user_id`={$data['user_id']} AND `status`=1"); // status 1 = berhasil terkirim
+		$unread   = intval($unread) + 1; // Untuk menampilkan badge total unread di icon aplikasinya. Dibuat +1 karena prosesnya curl dulu baru insert data ke tabel `bbc_user_push_notif` jadi untuk insert selanjutnya dianggap berhasil saja. Di mobile app dicek jika jumlah bagde notif != jumlah unread ini, maka akan diset sama dengan jumlah bagde notif
 		$tos      = array();
 		$last_id  = intval($last_id);
 		$updatedb = 0;
@@ -378,6 +380,7 @@ function alert_push_send($id, $last_id=0)
 						'title'     => $data['title'],
 						'body'      => $data['message'],
 						'sound'     => 'default',
+						'badge'     => $unread,
 						'channelId' => 'android',
 						'data'      => array(
 														'id'      => $data['id'],
