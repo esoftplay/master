cb2ef1085f79e838efee825b37fbce851ee1082f
Danang Widiantoro <danang@fisip.net>
2020-05-12 18:47:53 +0700

    .

--- modules/_cpanel/main.html.php
+++ modules/_cpanel/main.html.php
@@ -16,37 +16,40 @@ if(!empty($data))
 
 $data['image']  = "";
 
-$data['total'] = count($data_result['data']);
 
 $data['config'] = $data_result['config'];
 
 $r = array();
-foreach ($data_result['data'] as $d)
+if (is_array($data_result['data']))
 {
-	$r[] =array(
-		'id'          => $d['id'],
-		'title'       => $d['title'],
-		'intro'       => $d['content'],
-		'description' => '',
-		'image'       => content_src($d['image'], false, true),
-		'created'     => $d['created'],
-		'updated'     => $d['modified'],
-		'url'         => content_link($d['id'], $d['title']),
-		'publish'     => $d['publish'],
-		// additional info
-		// 'type_id'          => $d['type_id'],
-		// 'created_by'       => $d['created_by'],
-		// 'created_by_alias' => $d['created_by_alias'],
-		// 'modified_by'      => $d['modified_by'],
-		// 'revised'          => $d['revised'],
-		// 'hits'             => $d['hits'],
-		// 'rating'           => $d['rating'],
-		// 'last_hits'        => $d['last_hits'],
-		// 'is_popimage'      => $d['is_popimage'],
-		// 'is_front'         => $d['is_front'],
-		// 'is_config'        => $d['is_config'],
-		// 'config'           => $d['config'],
-		);
+	$data['total'] = count($data_result['data']);
+	foreach ($data_result['data'] as $d)
+	{
+		$r[] =array(
+			'id'          => $d['id'],
+			'title'       => $d['title'],
+			'intro'       => $d['content'],
+			'description' => '',
+			'image'       => content_src($d['image'], false, true),
+			'created'     => $d['created'],
+			'updated'     => $d['modified'],
+			'url'         => content_link($d['id'], $d['title']),
+			'publish'     => $d['publish'],
+			// additional info
+			// 'type_id'          => $d['type_id'],
+			// 'created_by'       => $d['created_by'],
+			// 'created_by_alias' => $d['created_by_alias'],
+			// 'modified_by'      => $d['modified_by'],
+			// 'revised'          => $d['revised'],
+			// 'hits'             => $d['hits'],
+			// 'rating'           => $d['rating'],
+			// 'last_hits'        => $d['last_hits'],
+			// 'is_popimage'      => $d['is_popimage'],
+			// 'is_front'         => $d['is_front'],
+			// 'is_config'        => $d['is_config'],
+			// 'config'           => $d['config'],
+			);
+	}
 }
 $data['list'] = $r;
 
